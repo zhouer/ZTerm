4e797e3d3b5afb6376acb7ce643e4f732a38737f
ericsk <ericsk@373988cc-8b32-0410-82f8-d14481cb1f8b>
2007-11-12 15:55:03 +0000

    Use Java Input Method Framework to process the carets from IME.
    On-the-spot rectangle has not been drawed yet.

--- org/zhouer/vt/User.java
+++ org/zhouer/vt/User.java
@@ -7,7 +7,24 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class User implements KeyListener, MouseListener, MouseMotionListener
+// add by ericsk 2007/11/12
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextAttribute;
+import java.awt.font.TextHitInfo;
+import java.awt.font.TextLayout;
+import java.awt.im.InputMethodRequests;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedCharacterIterator.Attribute;
+import java.text.AttributedString;
+
+
+public class User implements KeyListener, MouseListener, MouseMotionListener, InputMethodListener, InputMethodRequests
 {
 	private Application parent;
 	private VT100 vt;
@@ -180,6 +197,7 @@ public class User implements KeyListener, MouseListener, MouseMotionListener
 		
 		// 一般按鍵，直接送出
 		parent.writeChar( e.getKeyChar() );
+		e.consume();
 	}
 	
 	public void mouseClicked( MouseEvent e )
@@ -296,6 +314,171 @@ public class User implements KeyListener, MouseListener, MouseMotionListener
 		vt.setSelected( pressX, pressY, dragX, dragY );
 		vt.repaint();
 	}
+	
+	// add by ericsk 2007/11/12
+	private AttributedString composedTextString = null;
+	private AttributedCharacterIterator composedText = null;
+	private TextHitInfo caret = null;
+	private static final Attribute[] IM_ATTRIBUTES = { TextAttribute.INPUT_METHOD_HIGHLIGHT };	
+	private int textOriginX = 10;
+    private int textOriginY = 40;
+	private transient TextLayout textLayout = null;
+    private transient boolean validTextLayout = false;
+	private static final AttributedCharacterIterator EMPTY_TEXT = (new AttributedString("")).getIterator();
+	
+	public void inputMethodTextChanged( InputMethodEvent e) {
+		int committedCharacterCount = e.getCommittedCharacterCount();
+		AttributedCharacterIterator text = e.getText();
+		composedText = null;
+		char c;
+		if ( text != null ) {
+			int toCopy = committedCharacterCount;
+			c = text.first();
+			while ( toCopy-- > 0 ) {
+				parent.writeChar(c);
+				c = text.next();
+			}
+			
+			if (text.getEndIndex() - (text.getBeginIndex() + committedCharacterCount) > 0) {
+				composedTextString = new AttributedString(text,
+			                        text.getBeginIndex() + committedCharacterCount, // skip over committed text
+			                        text.getEndIndex(), IM_ATTRIBUTES);
+			    // add font information because TextLayout requires it
+			    composedTextString.addAttribute(TextAttribute.FONT, vt.getFont());
+			    composedText = composedTextString.getIterator();
+			}
+		}
+		e.consume();
+	}
+	
+	public void caretPositionChanged(InputMethodEvent event) {
+		caret = event.getCaret();
+		event.consume();
+	}
+	
+	public TextHitInfo getCaret() {
+		if (composedText == null) {
+			return TextHitInfo.trailing(committedText.length() - 1);
+		} else if (caret == null) {
+		    return null;
+		} else {
+		    // the caret provided by the input method is relative
+		    // to the composed text, so translate it to the entire text
+		    return caret.getOffsetHit(getCommittedTextLength());
+		}
+	}
+	
+	public Rectangle getCaretRectangle() {
+		TextHitInfo caret = getCaret();
+	    if (caret == null) {
+	    	return null;
+	    }
+	    return getCaretRectangle(caret);
+	}
+
+	public AttributedCharacterIterator getDisplayText() {
+		AttributedString string = new AttributedString(committedText.toString());
+		if (committedText.length() > 0) {
+			string.addAttribute(TextAttribute.FONT, vt.getFont());
+		}
+		return string.getIterator();
+	}
+	
+	public synchronized TextLayout getTextLayout() {
+		if (!validTextLayout) {
+			textLayout = null;
+		    AttributedCharacterIterator text = getDisplayText();
+		    if (text.getEndIndex() > text.getBeginIndex()) {
+		    	FontRenderContext context = ((Graphics2D) vt.getGraphics()).getFontRenderContext();
+		        textLayout = new TextLayout(text, context);
+		    }
+		}
+		validTextLayout = true;
+		return textLayout;
+	}
+	
+	public Rectangle getCaretRectangle(TextHitInfo caret) {
+		TextLayout textLayout = getTextLayout();
+	    int caretLocation;
+	    if (textLayout != null) {
+	    	caretLocation = Math.round(textLayout.getCaretInfo(caret)[0]);
+	    } else {
+	    	caretLocation = 0;
+	    }
+	    FontMetrics metrics = vt.getGraphics().getFontMetrics();
+	    return new Rectangle(textOriginX + caretLocation,
+	                         textOriginY - metrics.getAscent(),
+	                         0, metrics.getAscent() + metrics.getDescent());
+	}
+	
+	public Rectangle getTextLocation(TextHitInfo offset) {
+		// determine the text location in component coordinates
+		Rectangle rectangle;
+		if (offset == null) {
+			// no composed text: return caret for committed text
+		    rectangle = getCaretRectangle();
+		} else {
+		    // composed text: return caret within composed text
+		    TextHitInfo globalOffset = offset.getOffsetHit(getCommittedTextLength());
+		    rectangle = getCaretRectangle(globalOffset);
+		}
+
+		// translate to screen coordinates
+		Point location = vt.getLocationOnScreen();
+		rectangle.translate(location.x, location.y);
+
+		return rectangle;
+	}
+	
+	public Point getTextOrigin() {
+		return new Point(textOriginX, textOriginY);
+	}
+
+	public TextHitInfo getLocationOffset(int x, int y) {
+		// translate from screen coordinates to coordinates in the text layout
+	    Point location = vt.getLocationOnScreen();
+	    Point textOrigin = getTextOrigin();
+	    x -= location.x + textOrigin.x;
+	    y -= location.y + textOrigin.y;
+
+	    // TextLayout maps locations far outside its bounds to locations within.
+	    // To avoid false hits, we use it only if it actually contains the location.
+	    // We also have to translate the TextHitInfo to be relative to composed text.
+	    TextLayout textLayout = getTextLayout();
+	    if (textLayout != null && textLayout.getBounds().contains(x, y)) {
+	    	return textLayout.hitTestChar(x, y).getOffsetHit(-getCommittedTextLength());
+		} else {
+			return null;
+	    }
+	}
+
+	public int getInsertPositionOffset() {
+        return getCommittedTextLength();
+	}
+	
+	public AttributedCharacterIterator getCommittedText(int beginIndex, int endIndex) {
+		AttributedString string = new AttributedString(committedText.toString());
+	    return string.getIterator(null, beginIndex, endIndex);
+	}
+
+	public AttributedCharacterIterator getCommittedText(int beginIndex, int endIndex, Attribute[] attributes) {
+	    return getCommittedText(beginIndex, endIndex);
+	}
+
+	private StringBuffer committedText = new StringBuffer();
+
+	public int getCommittedTextLength() {
+		return committedText.length();
+	}
+
+	public AttributedCharacterIterator cancelLatestCommittedText(Attribute[] attributes) {
+		return null;
+	}
+	
+	public AttributedCharacterIterator getSelectedText(Attribute[] attributes) {
+		return EMPTY_TEXT;
+	}
+	// END of adding by ericsk
 	
 	public User( Application p, VT100 v, Config c )
 	{
